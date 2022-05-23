f6c9e140beeb817a270ec5300ee76e432b9afc64
Jaymeson Mendes <jaymesonmendes@gmail.com>
2022-05-22 23:23:05 -0300

    Commit

--- assets/script.js
+++ assets/script.js
@@ -1,49 +1,100 @@
+// Variável auxilar
+
 const baseURL = "http://localhost:3000";
 
-async function findAllPaletas() {
-  const res = await fetch(`${baseURL}/paletas/todas-paletas`);
+// Requisições
 
-  const paletas = await res.json();
-  paletas.forEach((paleta) => {
+const listarTodasAsPaletas = async () => {
+  const resposta = await fetch(`${baseURL}/paletas/todas-paletas`);
+  const paletas = await resposta.json();
+
+  return paletas;
+};
+
+const buscarPaletasPorId = async (id) => {
+  const resposta = await fetch(`${baseURL}/paletas/paleta/${id}`);
+  const paleta = await resposta.json();
+
+  return paleta;
+};
+
+const criarPaleta = async (sabor, descricao, foto, preco) => {
+  const paleta = {
+    sabor,
+    descricao,
+    foto,
+    preco,
+  };
+
+  const resposta = await fetch(`${baseURL}/paletas/criar-paleta`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    mode: "cors",
+    body: JSON.stringify(paleta),
+  });
+
+  const novaPaleta = await resposta.json();
+
+  return novaPaleta;
+};
+
+const atualizarPaleta = async (id, sabor, descricao, foto, preco) => {
+  const paleta = {
+    sabor,
+    descricao,
+    foto,
+    preco,
+  };
+
+  const resposta = await fetch(`${baseURL}/paletas/atualizar-paleta/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    mode: "cors",
+    body: JSON.stringify(paleta),
+  });
+  const paletaAtualizada = await resposta.json();
+
+  return paletaAtualizada;
+};
+
+const excluirPaleta = async (id) => {
+  const resposta = await fetch(`${baseURL}/paletas/excluir-paleta/${id}`, {
+    method: "DELETE",
+    mode: "cors",
+  });
+
+  if (resposta.status === 204) {
+    return "Paleta excuída com sucesso";
+  } else {
+    return "Paleta não encontrada";
+  }
+};
+
+// Manipulação do DOM
+
+const imprimirTodasAsPaletas = async () => {
+  const paletas = await listarTodasAsPaletas();
+
+  paletas.forEach((element) => {
     document.getElementById("paletaList").insertAdjacentHTML(
       "beforeend",
       `
-      <div class="PaletaListaItem">
-        <div>
-            <div class="PaletaListaItem__sabor">${paleta.sabor}</div>
-            <div class="PaletaListaItem__preco">R$ ${paleta.preco}</div>
-            <div class="PaletaListaItem__descricao">${paleta.descricao}</div>
+      <div class="CartaoPaleta">
+        <div class="CartaoPaleta__infos">
+           <h4>${element.sabor}</h4>
+           <span>R$ ${element.preco.toFixed(2)}</span>
+           <P>${element.descricao}</P>
         </div>
-        <img class="PaletaListaItem__foto" src="${paleta.foto}" alt="Paleta de ${paleta.sabor}" />
+        <img class="CartaoPaleta_foto" src="${element.foto}" alt="Paleta de ${element.sabor}" />
     </div>
-        
-        
+
         `
     );
   });
-}
+};
 
-async function findByIdPaletas() {
-    const id = document.getElementById("idPaleta").value
-
-    const response = await fetch(`${baseURL}/paletas/paleta/${id}`)
-    const paleta = await response.json();
-
-    const paletaEscolhidaDiv = document.getElementById("paletaEscolhida")
-    paletaEscolhidaDiv.innerHTML = ` 
-    <div class="PaletaCardItem">
-    <div>
-        <div class="PaletaCardItem__sabor">${paleta.sabor}</div>
-        <div class="PaletaCardItem__preco">R$ ${paleta.preco}</div>
-        <div class="PaletaCardItem__descricao">${paleta.descricao}</div>
-    </div>
-    <img class="PaletaCardItem__foto" src="${paleta.foto}" alt="Paleta de ${paleta.sabor}" />
-</div>
-`
-
-}
-
-
-findAllPaletas();
-findByIdPaletas();
-
+imprimirTodasAsPaletas()
